d008864d22710b3bd154e795649a2cddaea3e8df
kartikr <rkartik1992@gmail.com>
2020-05-01 17:29:17 +0530

    fix lint error

--- angular8-springboot-client/src/app/update-employee/update-employee.component.ts
+++ angular8-springboot-client/src/app/update-employee/update-employee.component.ts
@@ -6,37 +6,40 @@ import { EmployeeService } from '../employee.service';
 @Component({
   selector: 'app-update-employee',
   templateUrl: './update-employee.component.html',
-  styleUrls: ['./update-employee.component.css']
+  styleUrls: ['./update-employee.component.css'],
 })
 export class UpdateEmployeeComponent implements OnInit {
-
   id: number;
   employee: Employee;
+  submitted: boolean;
 
-  constructor(private route: ActivatedRoute,private router: Router,
-    private employeeService: EmployeeService) { }
+  constructor(private route: ActivatedRoute, private router: Router, private employeeService: EmployeeService) {}
 
   ngOnInit() {
     this.employee = new Employee();
 
     this.id = this.route.snapshot.params['id'];
-    
-    this.employeeService.getEmployee(this.id)
-      .subscribe(data => {
-        console.log(data)
+
+    this.employeeService.getEmployee(this.id).subscribe(
+      (data) => {
+        console.log(data);
         this.employee = data;
-      }, error => console.log(error));
+      },
+      (error) => console.log(error)
+    );
   }
 
   updateEmployee() {
-    this.employeeService.updateEmployee(this.id, this.employee)
-      .subscribe(data => console.log(data), error => console.log(error));
+    this.employeeService.updateEmployee(this.id, this.employee).subscribe(
+      (data) => console.log(data),
+      (error) => console.log(error)
+    );
     this.employee = new Employee();
     this.gotoList();
   }
 
   onSubmit() {
-    this.updateEmployee();    
+    this.updateEmployee();
   }
 
   gotoList() {
